9f0729caf36fcc8af3c54498ae8d25811fb395de
SynthwaveFox <soudert1@hotmail.com>
2025-04-01 22:30:24 -0400

    the server commands are now for whitelisted users

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,12 +1,17 @@
-import fs from 'fs';  // To read command files dynamically
+import fs from 'fs';
 
-const once = false; // You can keep this as false if you want it to be non-episodic
+const once = false;
 const name = 'interactionCreate';
+
+// Define a list of whitelisted user IDs
+const WHITELISTED_USER_IDS = ['329411181582483456', '134775482930429952', '289136609520254976', '574609789653483521']; // Replace with actual user IDs
 
 async function invoke(interaction) {
     // Check if the interaction is a command
     if (interaction.isChatInputCommand()) {
-        // Dynamically load the command file based on the command name
+            if (!WHITELISTED_USER_IDS.includes(interaction.user.id)) {
+                return interaction.reply('Sorry, you are not authorized to use this command');
+            }
         try {
             // Dynamically import the command file based on the command name
             const commandFile = await import(`../events/commands/${interaction.commandName}.js`);
