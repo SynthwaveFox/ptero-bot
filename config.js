326b46ab7ddff39bb896ab23303b01ca9e662d7f
SynthwaveFox <soudert1@hotmail.com>
2025-04-18 13:59:14 -0400

    updated server IDs

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export const SERVER_IDS = [
-    '2cf9a7ff-d770-4649-86fb-617cb08e10b6',
-    '5c0b3278-3d1b-4faa-b532-705f1f83516d',
-    '17f4e985-dae0-4c8c-97a1-a352f9fdbcad'
+  '3c6f0a7b-ad7c-492e-81dc-6675c5ed51c0',
+  '1324cdbd-c523-4816-9cce-c0f19cd9e06a',
+  'abf86b0f-44e0-4768-b133-7efc73a86f06'
   ];
   
   export const SERVER_NAMES = {
-    '2cf9a7ff-d770-4649-86fb-617cb08e10b6': 'Minecraft-Forge',
-    '5c0b3278-3d1b-4faa-b532-705f1f83516d': 'Minecraft-Vanilla',
-    '17f4e985-dae0-4c8c-97a1-a352f9fdbcad': 'Satisfactory'
+    '1324cdbd-c523-4816-9cce-c0f19cd9e06a': 'Minecraft-Forge',
+    '3c6f0a7b-ad7c-492e-81dc-6675c5ed51c0': 'Minecraft-Vanilla',
+    'abf86b0f-44e0-4768-b133-7efc73a86f06': 'Satisfactory'
   };
